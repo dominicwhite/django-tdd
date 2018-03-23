f2702594c00c94bc0df358f0d6d163c357cf6591
Dominic White <dewhite@gmail.com>
2018-03-23 07:59:01 -0400

    Refactor list view to handle new item POSTs

--- superlists_project/lists/tests/test_views.py
+++ superlists_project/lists/tests/test_views.py
@@ -22,7 +22,7 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/'+str(list_.id)+'/')
         self.assertTemplateUsed(response, 'list.html')
     
-    def test_displays_all_items(self):
+    def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
         Item.objects.create(text='itemey 2', list=correct_list)
@@ -41,8 +41,61 @@ class ListViewTest(TestCase):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get('/lists/'+str(correct_list.id)+'/')
-        self.assertEqual(response.context['list'], correct_list)  
-        
+        self.assertEqual(response.context['list'], correct_list) 
+    
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/'+str(correct_list.id)+'/',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/'+str(correct_list.id)+'/',
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertRedirects(response, '/lists/'+str(correct_list.id)+'/')
+    
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/'+str(correct_list.id)+'/',
+            data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+
+    def test_POST_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/'+str(correct_list.id)+'/',
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertRedirects(response, '/lists/'+str(correct_list.id)+'/')
+
+
+
 class NewListTest(TestCase):
     
     def test_can_save_a_POST_request(self):
@@ -69,33 +122,6 @@ class NewListTest(TestCase):
         self.assertEqual(Item.objects.count(), 0)
 
 
-class NewItemTest(TestCase):
-
-    def test_can_save_a_POST_request_to_an_existing_list(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        self.client.post(
-            '/lists/'+str(correct_list.id)+'/add_item',
-            data={'item_text': 'A new item for an existing list'}
-        )
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new item for an existing list')
-        self.assertEqual(new_item.list, correct_list)
-
-
-    def test_redirects_to_list_view(self):
-        other_list = List.objects.create()
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/'+str(correct_list.id)+'/add_item',
-            data={'item_text': 'A new item for an existing list'}
-        )
-
-        self.assertRedirects(response, '/lists/'+str(correct_list.id)+'/')
 
 
 
--- superlists_project/lists/views.py
+++ superlists_project/lists/views.py
@@ -10,6 +10,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/'+str(list_.id)+'/')
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
@@ -19,11 +22,8 @@ def new_list(request):
         item.full_clean()
         item.save()
     except ValidationError:
+        list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/'+str(list_.id)+'/')
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/'+str(list_.id)+'/')
